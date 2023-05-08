2c6402e4cf9553e8c7ccba67578c81470fbcae28
Cl0udKiD <sasha.azarov8@gmail.com>
2023-05-08 16:37:39 +0300

    fix: fixed issues with removal of files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,34 +5,35 @@ const dest = './04-copy-directory/files-copy'
 
 
 function copyDir(src,dest){
-    fs.readdir(src, (err, files) => {
-        fs.exists(dest,(exists)=>{
-            if(!exists) 
-            {
-                fs.mkdir(dest,{recursive: true},(err)=>{
-                    if (err) console.log(err)
-                    files.forEach(file => {
-                        fs.copyFile(`${src}/${file}`,`${dest}/${file}`,(err)=>{
-                            if(err)console.log(err)
-                        })
-                    })
-                });
-            }else{
-                fs.readdir(dest,(err,files)=>{
-                    files.forEach(file=>{
-                        fs.unlink(`${dest}/${file}`,(err)=>{
-                            if(err) console.log(err)
-                        })
-                    })
-                })
+    fs.exists(dest,(exists)=>{
+        if(!exists){
+            fs.mkdir(dest,{recursive: true},(err)=>{
+                if (err) console.log(err)
+            })
+            fs.readdir(src, (err, files) => {
                 files.forEach(file => {
                     fs.copyFile(`${src}/${file}`,`${dest}/${file}`,(err)=>{
                         if(err)console.log(err)
                     })
                 })
-            }
-        })
-    });
+            })
+        }else{
+            fs.readdir(dest,(err,files)=>{
+                files.forEach(file=>{
+                    fs.unlink(`${dest}/${file}`,(err)=>{
+                        if(err) console.log(err)
+                    })
+                })
+                fs.readdir(src, (err, files) => {
+                    files.forEach(file => {
+                        fs.copyFile(`${src}/${file}`,`${dest}/${file}`,(err)=>{
+                            if(err)console.log(err)
+                        })
+                    })
+                })
+            })
+        }
+    })
 }
 
 copyDir(src, dest)
